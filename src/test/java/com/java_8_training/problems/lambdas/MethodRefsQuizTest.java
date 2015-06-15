9e599b53df42fa4fe01c821438548fdf76b8caa8
Colin Kennedy <colin.kennedy@teamaol.com>
2015-06-15 10:54:40 +0100

    Refactor to use method references

--- src/test/java/com/java_8_training/problems/lambdas/MethodRefsQuizTest.java
+++ src/test/java/com/java_8_training/problems/lambdas/MethodRefsQuizTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.Matchers.is;
@@ -19,7 +20,7 @@ public class MethodRefsQuizTest {
     @Test
     public void stringToIntegerQuiz(){
 
-        Function<String, Integer> stringToInteger = (String s) -> Integer.parseInt(s);
+        Function<String, Integer> stringToInteger = Integer::parseInt;
 
         assertThat(2, is(stringToInteger.apply("2")));
 
@@ -29,8 +30,10 @@ public class MethodRefsQuizTest {
     @Test
     public void containsQuiz(){
 
-        BiPredicate<List<String>, String> contains = (list, element) -> list.contains(element);
-
+        BiPredicate<List<String>, String> contains = List::contains;
+        List<String> list = Arrays.asList("who", "how", "why");
+//        list = list.parallelStream().map(String::toUpperCase).collect(Collectors.toList());
+//        System.out.println(list);
         boolean doesItContainIt = contains.test(Arrays.asList("who", "how", "why"), "who");
 
         assertEquals(true, doesItContainIt);
